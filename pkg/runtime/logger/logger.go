b04fc271ad0365539ad0d2d3aa129b156d2142c0
Li Den <malivenlipton@gmail.com>
2025-12-01 17:44:30 +0300

    corrected test

--- pkg/runtime/logger/logger.go
+++ pkg/runtime/logger/logger.go
@@ -15,7 +15,7 @@ type HTTPLogger struct {
 
 func NewHTTPLogger() *HTTPLogger {
 	// Создаем папку runtime/log
-	logDir := "pkg/runtime/log"
+	logDir := "runtime/log"
 	if err := os.MkdirAll(logDir, 0755); err != nil {
 		panic("failed to create log directory: " + err.Error())
 	}
